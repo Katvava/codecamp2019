f12ecd1d1567c0e85fa21b038bb9baa30a341d24
nuochen <nuo.chen@borrowell.com>
2018-11-03 14:21:08 -0400

    removed unused code and added a second solution with dictioanry

--- python3/two_sum_nuo.py
+++ python3/two_sum_nuo.py
@@ -1,21 +1,30 @@
+# 1
 # Given an array of integers, return indices of the two numbers such that they
 # add up to a specific target.
 # You may assume that each input would have exactly one solution
 # and you may not use the same element twice
 
+# Solution 1 (nested for loops, O(N^2))
+
 def two_sum(l, target):
 
     i = 0
 
-    if len(l) < 2:
-        print('invalid input')
-    elif len(l) == 2:
-        return 0, 1
-    else:
-        for i in range(len(l)):
-            for j in range(i + 1, len(l)):
-                if l[i] + l[j] == target:
-                    return i, j
-                else:
-                    j += 1
-            i += 1
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] + l[j] == target:
+                return i, j
+            else:
+                j += 1
+        i += 1
+
+# Soulution 2 (dictionary, O(N))
+
+def two_sum(l, target):
+
+    d = {}
+    for i in range(len(l)):
+        if target - l[i] in d:
+            return i, d[target - l[i]]
+        else:
+            d[l[i]] = i
